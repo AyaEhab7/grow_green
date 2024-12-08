80a230c453ce42beba0686d0a4779cc3ad5c87a4
loly1710 <loly15710@gmail.com>
2024-12-08 14:10:13 +0300

    add db

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+NURSERIES = (
+    ('Tree Nurseries', 'Tree Nurseries'),
+    ('Vegetable Nurseries', 'Vegetable Nurseries'),
+    ('Ornmental Nurseries', 'Ornmental Nurseries')
+)
+
+
+class Nurseries(models.Model):
+    category = models.CharField(max_length=50, choices= NURSERIES )
+    file = models.TextField(max_length=1000)
+    def __str__(self):
+        return self.category
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
-
+from .models import Nurseries
 from django.http import HttpResponse
 
 
-class Nurseries:
-    def __init__(self, name, image):
-        self.name = name
-        self.image = image
-
-nurseries =[
-    Nurseries('tree','https://img.freepik.com/free-photo/tree-with-white-background_1232-574.jpg?t=st=1733642842~exp=1733646442~hmac=7fa5cefa1763ede182a880bd1c5d9f7ff62cdf9b7e39b54cf12dc2b62fa50ac9&w=740'),
-    Nurseries('vegetable','https://img.freepik.com/free-photo/green-broccoli-levitating-white-background_485709-79.jpg?t=st=1733642873~exp=1733646473~hmac=186afc585dc73cd937dbb208dd078bb2dd9517cc500790d13f80f28a0e158397&w=360'),
-    Nurseries('ornamental','https://img.freepik.com/free-photo/large-set-isolated-vegetables-white-background_485709-44.jpg?t=st=1733642907~exp=1733646507~hmac=9a05489191b93f27ba3b174eee541edd6a7459493d577c5f199da7d64ab94182&w=900'),
-]        
-
+        
 def home(request):
     return render(request, 'home.html')
 
@@ -21,4 +11,5 @@ def about(request):
     return render(request, 'about.html')
 
 def nurseries_index(request):
+    nurseries = Nurseries.objects.all()
     return render(request, 'nurseries/index.html', {'nurseries': nurseries})

deda677f3abc60f961f095c4385dd0f28eceea26
loly1710 <loly15710@gmail.com>
2024-12-09 16:49:28 +0300

    fix2

--- main_app/views.py
+++ main_app/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import IrrigationForm, FertilizationForm, PestControlForm
 #from django.http import HttpResponse
 from django import forms
+from django.urls import reverse_lazy
 from .forms import ProductRequestForm
 
 @login_required

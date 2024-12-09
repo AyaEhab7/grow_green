e89f6a5cdb63f93cbab9bd84e570c5f5e981b4b7
Shahstaa <sh.sabraa@gmail.com>
2024-12-09 14:45:30 +0300

    done with edit&delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('nurseries/', views.nurseries_index, name='nurseries-index'),
     path('nurseries/<int:nurserie_id>/', views.plant_list, name='plant-list'),
     path('store/', views.store, name='store'),
+    path('store/product/create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('store/product/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='product-edit'),
+    path('store/product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product-delete'),
     path('product/<int:id>/detail/', views.product_detail, name='product-detail'),
-    
     path('request_product/<int:product_id>/', views.product_request, name='product-request'),
+
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,19 @@
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django import forms
+
 from .models import Nurseries , Plants
 from .models import Product, ProductRequest
+
 from .forms import ProductRequestForm
-from django.http import HttpResponse
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+@login_required
+def product_create(request):
+    if not request.user.is_superuser: 
+        return redirect('store')
       
 def home(request):
     return render(request, 'home.html')
@@ -40,3 +50,36 @@ def product_request(request, product_id):
 def product_detail(request, id):
     product = Product.objects.get(id=id)  
     return render(request, 'store/product_detail.html', {'product': product})
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'description', 'price', 'quantity', 'category', 'image_url']
+    template_name = 'store/product_form.html'
+    success_url = '/store'
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'description', 'price', 'quantity', 'category', 'image_url']
+    template_name = 'store/product_form.html'
+    success_url = '/store' 
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'store/product_confirm_delete.html'
+    success_url = '/store'
+
+    def get_success_url(self):
+        return reverse_lazy('store') 
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'quantity', 'category', 'image_url']
+
+
+
+
+
+
+

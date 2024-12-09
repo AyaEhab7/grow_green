30b697dac708d0f22429894f045ef479291cc894
loly1710 <loly15710@gmail.com>
2024-12-09 16:09:31 +0300

    donePlants

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Nurseries, Plants
+from .models import Nurseries, Plants, Irrigation, Fertilization, PestControl
 
 admin.site.register(Nurseries)
 admin.site.register(Plants)
+admin.site.register(Irrigation)
+admin.site.register(Fertilization)
+admin.site.register(PestControl)
 
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('nurseries/', views.nurseries_index, name='nurseries-index'),
     path('nurseries/<int:nurserie_id>/', views.plant_list, name='plant-list'),
+    path('plants/<int:plant_id>/', views.plant_detail, name='plant_detail'),
+    path('plants/create/', views.PlantCreate.as_view(), name='plant-create'),
+    path('plants/<int:pk>/update/', views.PlantUpdate.as_view(), name='plant-update'),
+    path('plants/<int:pk>/delete/', views.PlantDelete.as_view(), name='plant-delete'),
+    path('plants/<int:plant_id>/add/', views.add_nursery_care, name='add_nursery_care'),
 ]

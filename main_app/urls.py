c0ca4cf5126ae297e03bb7682127144fbf141105
aya ehab <ayaaehab1999@gmail.com>
2024-12-09 11:28:43 +0300

    add details to the store

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('nurseries/', views.nurseries_index, name='nurseries-index'),
     path('nurseries/<int:nurserie_id>/', views.plant_list, name='plant-list'),
     path('store/', views.store, name='store'),
+    path('product/<int:id>/detail/', views.product_detail, name='product-detail'),
+    
     path('request_product/<int:product_id>/', views.product_request, name='product-request'),
 ]
